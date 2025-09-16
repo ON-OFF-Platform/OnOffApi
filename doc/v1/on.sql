97a00e3d2c80a7c220659484b626520bb1adeff2
hyeon0114 <kshyeon114@gmail.com>
2025-09-16 09:20:17 +0900

    [SN-0008] [API] 이메일 인증번호 전송 기능 추가
    - DB 수정

--- doc/v1/on.sql
+++ doc/v1/on.sql
@@ -33,70 +33,86 @@ CREATE TABLE `terms` (
   PRIMARY KEY (`terms_id`)
 );
 
-CREATE TABLE `user_terms_agree` (
-  `user_terms_agree_id` INT AUTO_INCREMENT COMMENT '사용자 동의 약관 ID',
-  `user_id` VARCHAR(255) NOT NULL COMMENT '사용자 ID',
-  `terms_id` INT NOT NULL COMMENT '동의 약관 ID',
-  `agreed` TINYINT(1) NOT NULL DEFAULT 1 COMMENT '동의 여부',
+CREATE TABLE `terms` (
+  `terms_id` INT AUTO_INCREMENT COMMENT '동의 약관 ID',
+  `type` ENUM('SERVICE', 'PRIVACY', 'MARKETING') NOT NULL COMMENT '동의 약관 타입',
+  `title` VARCHAR(255) NOT NULL COMMENT '동의 약관 제목',
+  `content` LONGTEXT NOT NULL COMMENT '약관 전문 (긴 텍스트)',
+  `required` ENUM('Y', 'N') NOT NULL DEFAULT 'Y' COMMENT '약관 동의 필수 여부',
+  `version` DATE NOT NULL COMMENT '버전 관리, YYYY-MM-dd',
   `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT '생성일',
   `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정일자',
-  FOREIGN KEY (`terms_id`) REFERENCES terms(`terms_id`),
-  PRIMARY KEY (`user_terms_agree_id`)
+  PRIMARY KEY (`terms_id`)
 );
 
-
 CREATE TABLE `schedules` (
-    schedule_id INT AUTO_INCREMENT PRIMARY KEY, -- 일정 고유 ID
-    user_id INT NOT NULL,                       -- 일정 소유자
-    title VARCHAR(100) NOT NULL,                -- 일정 제목
-    description TEXT,                           -- 일정 내용
-    start_time DATETIME NOT NULL,               -- 시작 시간
-    end_time DATETIME NOT NULL,                 -- 종료 시간
-    is_all_day BOOLEAN DEFAULT FALSE,           -- 종일 여부
-    location VARCHAR(200),                      -- 장소
-    color VARCHAR(20),                          -- 캘린더 표시 색상
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES user(user_id)
+  `schedule_id` VARCHAR(255) NOT NULL COMMENT '일정 고유 ID',
+  `user_id` VARCHAR(255) NOT NULL COMMENT '사용자 ID',
+  `schedule_ctg_id` INT NOT NULL COMMENT '일정 카테고리 ID',
+  `content` VARCHAR(255) NOT NULL COMMENT '일정 내용',
+  `start_time` DATETIME NOT NULL COMMENT '시작 시간',
+  `end_time` DATETIME NOT NULL COMMENT '종료 시간',
+  `is_all_day` ENUM('Y', 'N') DEFAULT 'N' NULL COMMENT '종일 여부',
+  `ctg_name` VARCHAR(255) NOT NULL COMMENT '카테고리 이름',
+  `location` VARCHAR(200) NULL COMMENT '장소',
+  `created_  at` DATETIME NULL DEFAULT CURRENT_TIMESTAMP COMMENT '생성일자',
+  `updated_at` DATETIME NULL DEFAULT CURRENT_TIMESTAMP COMMENT '수정일자',
+  PRIMARY KEY (`schedule_id`),
+  FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
+  FOREIGN KEY (`schedule_ctg_id`) REFERENCES `schedule_ctg` (`schedule_ctg_id`)
+);
+
+CREATE TABLE `schedule_ctg` (
+  `schedule_ctg_id` INT NOT NULL COMMENT '일정 카테고리 고유 ID',
+  `user_id` VARCHAR(255) NOT NULL COMMENT '사용자 ID',
+  `name` VARCHAR(50) NOT NULL COMMENT '카테고리 이름',
+  `sort_order` INT NOT NULL COMMENT '순서',
+  `color_code` VARCHAR(7) NOT NULL DEFAULT '#81B792' COMMENT '색상 코드',
+  `created_at` DATETIME NULL DEFAULT CURRENT_TIMESTAMP COMMENT '생성일자',
+  `updated_at` DATETIME NULL DEFAULT CURRENT_TIMESTAMP COMMENT '수정일자',
+  PRIMARY KEY (`schedule_ctg_id`),
+  FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
 );
 
 CREATE TABLE `planned_expenses` (
-    planned_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL,
-    category_id INT NOT NULL,         -- 예상 지출 카테고리
-    schedule_id INT NULL,
-    title VARCHAR(100) NOT NULL,      -- 항목 이름
-    expected_amount DECIMAL(10,2) NOT NULL, -- 예상 금액
-    planned_date DATE NOT NULL,       -- 예상 날짜
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(user_id),
-    FOREIGN KEY (schedule_id) REFERENCES schedules(schedule_id),
-    FOREIGN KEY (category_id) REFERENCES expense_categories(category_id)
-);
-
-CREATE TABLE `expense_categories` (
-    category_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL,
-    name VARCHAR(50) NOT NULL,
-    type ENUM('income','expense') DEFAULT 'expense',
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
+  `planned_expenses_id` VARCHAR(255) NOT NULL COMMENT '예상 지출 고유 ID',
+  `schedule_id` VARCHAR(255) NOT NULL COMMENT '일정 고유 ID',
+  `user_id` VARCHAR(255) NOT NULL COMMENT '사용자 ID',
+  `expense_ctg_id` INT NOT NULL COMMENT '지출 카테고리 고유 ID',
+  `title` VARCHAR(100) NOT NULL COMMENT '예상 지출 제목',
+  `amount` DECIMAL(10,2) NOT NULL COMMENT '예상 지출 금액',
+  `expense_date` DATE NOT NULL COMMENT '예상 지출 날짜',
+  `created_at` DATETIME NULL DEFAULT CURRENT_TIMESTAMP COMMENT '생성일자',
+  `updated_at` DATETIME NULL DEFAULT CURRENT_TIMESTAMP COMMENT '수정일자',
+  PRIMARY KEY (`planned_expenses_id`),
+  FOREIGN KEY (`schedule_id`) REFERENCES `schedules` (`schedule_id`),
+  FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
+  FOREIGN KEY (`expense_ctg_id`) REFERENCES `expense_ctg` (`expense_ctg_id`)
 );
 
 CREATE TABLE `expenses` (
-    expense_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL,
-    category_id INT NOT NULL,
-    title VARCHAR(100) NOT NULL,
-    amount DECIMAL(10,2) NOT NULL,
-    expense_date DATE NOT NULL,
-    description TEXT,
-    schedule_id INT NULL,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(user_id),
-    FOREIGN KEY (category_id) REFERENCES expense_categories(category_id),
-    FOREIGN KEY (schedule_id) REFERENCES schedules(schedule_id)
+  `expense_id` VARCHAR(255) NOT NULL COMMENT '지출내역 고유 ID',
+  `user_id` VARCHAR(255) NOT NULL COMMENT '사용자 ID',
+  `expense_ctg_id` INT NOT NULL COMMENT '지출 카테고리 고유 ID',
+  `title` VARCHAR(100) NOT NULL COMMENT '지출 제목',
+  `amount` DECIMAL(10,2) NOT NULL COMMENT '지출 금액',
+  `expense_date` DATE NOT NULL COMMENT '지출 날짜',
+  `description` TEXT NULL COMMENT '지출 내용',
+  `created_at` DATETIME NULL DEFAULT CURRENT_TIMESTAMP COMMENT '생성일자',
+  `updated_at` DATETIME NULL DEFAULT CURRENT_TIMESTAMP COMMENT '수정일자',
+  PRIMARY KEY (`expense_id`),
+  FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
+  FOREIGN KEY (`expense_ctg_id`) REFERENCES `expense_ctg` (`expense_ctg_id`)
+);
+
+CREATE TABLE `expense_ctg` (
+  `expense_ctg_id` INT NOT NULL COMMENT '지출 카테고리 고유 ID',
+  `user_id` VARCHAR(255) NOT NULL COMMENT '사용자 ID',
+  `name` VARCHAR(50) NOT NULL COMMENT '카테고리 이름',
+  `type` ENUM('INCOME','EXPENSE') NOT NULL DEFAULT 'EXPENSE' COMMENT '타입',
+  `color_code` VARCHAR(7) NULL DEFAULT '#81B792' COMMENT '색상코드',
+  `created_at` DATETIME NULL DEFAULT CURRENT_TIMESTAMP COMMENT '생성일자',
+  `updated_at` DATETIME NULL DEFAULT CURRENT_TIMESTAMP COMMENT '수정일자',
+  PRIMARY KEY (`expense_ctg_id`),
+  FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
 );
